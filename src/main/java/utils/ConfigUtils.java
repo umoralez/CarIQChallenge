226d0ce9512713e5a682f537ebb1952870a4efad
Ulises Moralez <umoralez@solvd.com>
2024-01-24 08:41:58 -0300

    Update ConfigUtils and EndpointUtils

--- src/main/java/utils/ConfigUtils.java
+++ src/main/java/utils/ConfigUtils.java
@@ -1,2 +1,34 @@
-package utils;public class ConfigUtils {
+package utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class ConfigUtils {
+    private static final String CONFIG_FILE = "config.properties";
+    private static final Properties properties = loadProperties();
+
+    private static Properties loadProperties() {
+        Properties props = new Properties();
+        try (InputStream input = ConfigUtils.class.getClassLoader().getResourceAsStream(CONFIG_FILE)) {
+            props.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return props;
+    }
+
+    public static String getBaseUrl() {
+        return properties.getProperty("base.url");
+    }
+
+    public static int getApiTimeout() {
+        return Integer.parseInt(properties.getProperty("api.timeout"));
+    }
+
+    public static String readJsonFile(String filePath) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
 }
--- src/main/java/utils/EndpointUtils.java
+++ src/main/java/utils/EndpointUtils.java
@@ -1,2 +1,24 @@
-package utils;public class EndpointUtils {
+package utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class EndpointUtils {
+    private static final String ENDPOINTS_FILE = "endpoint.properties";
+    private static final Properties endpoints = loadEndpoints();
+
+    private static Properties loadEndpoints() {
+        Properties props = new Properties();
+        try (InputStream input = EndpointUtils.class.getClassLoader().getResourceAsStream(ENDPOINTS_FILE)) {
+            props.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return props;
+    }
+
+    public static String getEndpoint(String key) {
+        return endpoints.getProperty(key);
+    }
 }
