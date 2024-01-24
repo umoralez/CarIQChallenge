67c84af1415db7c0c2e9efaa0ff6cb7b46149f0a
Ulises Moralez <umoralez@solvd.com>
2024-01-24 08:42:12 -0300

    Update PetService

--- src/main/java/api/PetService.java
+++ src/main/java/api/PetService.java
@@ -1,2 +1,40 @@
-package api;public class PetService {
+package api;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import client.ApiClient;
+import client.ApiClientOkHTTP;
+import client.ApiClientRestAssured;
+import utils.EndpointUtils;
+
+public class PetService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PetService.class);
+    private final ApiClient apiClient;
+
+    public PetService(ApiClientOkHTTP apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    public PetService(ApiClientRestAssured apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    public String getSoldPets() throws IOException {
+        LOGGER.info("Get sold pets");
+        return apiClient.get(EndpointUtils.getEndpoint("pet.findByStatus"));
+    }
+
+    public String postSoldPets(String body) throws IOException {
+        LOGGER.info("Post sold pets");
+        return apiClient.post(EndpointUtils.getEndpoint("pet.post"), body);
+    }
+
+    public String putSoldPets(String body) throws IOException {
+        LOGGER.info("Put sold pets");
+        return apiClient.put(EndpointUtils.getEndpoint("pet.put"), body);
+    }
 }
